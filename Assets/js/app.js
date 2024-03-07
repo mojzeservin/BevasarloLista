17b1f08ca5e3010a02fdc0aa2082eb41807d6db0
MszrsRicsi <78954828+MszrsRicsi@users.noreply.github.com>
2024-03-07 14:08:50 +0100

    oldal letisztítása és kategória lista feltöltése

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,16 +1,28 @@
 let app = angular.module('bevasarlolistaApp', []);
 
+let kategoria = document.querySelector("#kategoria");
+let termek = document.querySelector("#termek");
+let mennyiseg = document.querySelector("#mennyiseg");
+
 app.run(function($rootScope)
 {
     $rootScope.appTitle =  "Bevásárló lista";
     $rootScope.author = "MR - ME";
-    $rootScope.company = "(12.A) Bajai SZC Türr István Technikum";
-    $rootScope.year = new Date().getFullYear();
 
     $rootScope.termekek = [];
 
+    $rootScope.kategoriak = [];
+
     axios.get("http://localhost:3000").then(res => {
         $rootScope.termekek = res.data;
+
+        for (let index = 0; index < $rootScope.termekek.length; index++) {
+            if (!$rootScope.kategoriak.includes($rootScope.termekek[index].category))
+            {
+                $rootScope.kategoriak.push($rootScope.termekek[index].category);
+            }
+        }
+
         $rootScope.$apply();
     });
 }); 
