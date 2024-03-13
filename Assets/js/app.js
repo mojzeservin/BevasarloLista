224735c4b40699294f9bea56886087ff9bcffc72
MszrsRicsi <78954828+MszrsRicsi@users.noreply.github.com>
2024-03-13 10:00:36 +0100

    termék felvétel funkció és combobox-ok feltöltése

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,17 +1,23 @@
 let app = angular.module('bevasarlolistaApp', []);
 
-let kategoria = document.querySelector("#kategoria");
-let termek = document.querySelector("#termek");
-let mennyiseg = document.querySelector("#mennyiseg");
+let kategoriaSelect = document.querySelector("#kategoriaSelect");
+let termekSelect = document.querySelector("#termekSelect");
+let mennyisegUpDown = document.querySelector("#mennyisegUpDown");
+let addBtn = document.querySelector(".addBtn");
 
 app.run(function($rootScope)
 {
     $rootScope.appTitle =  "Bevásárló lista";
+
     $rootScope.author = "MR - ME";
+
+    $rootScope.kategoriak = [];
 
     $rootScope.termekek = [];
 
-    $rootScope.kategoriak = [];
+    $rootScope.termekekNevei = [];
+
+    $rootScope.lista = [];
 
     axios.get("http://localhost:3000").then(res => {
         $rootScope.termekek = res.data;
@@ -23,6 +29,57 @@ app.run(function($rootScope)
             }
         }
 
+        for (let index = 0; index < $rootScope.kategoriak.length; index++) {
+            let option = document.createElement("option");
+            option.text = $rootScope.kategoriak[index];
+            kategoriaSelect.appendChild(option);
+        }
+
         $rootScope.$apply();
     });
-}); 
+
+
+    kategoriaSelect.addEventListener("click", ()=>
+    {
+        termekSelect.length = 1;
+
+        $rootScope.termekekNevei = [];
+
+        for (let index = 0; index < $rootScope.termekek.length; index++) {
+
+            if (kategoriaSelect.options[kategoriaSelect.selectedIndex].text == $rootScope.termekek[index].category && !$rootScope.termekekNevei.includes($rootScope.termekek[index].productname))
+            {
+                $rootScope.termekekNevei.push($rootScope.termekek[index].productname);
+            }
+        }
+
+        for (let index = 0; index < $rootScope.termekekNevei.length; index++) {
+            let option = document.createElement("option");
+            option.text = $rootScope.termekekNevei[index];
+            termekSelect.appendChild(option);
+        }
+    });
+
+    addBtn.addEventListener("click", () => {
+        if (kategoriaSelect.selectedIndex != 0 && termekSelect.selectedIndex != 0 && mennyisegUpDown.value > 0)
+        {
+            let termek = {
+                kategoria: kategoriaSelect.options[kategoriaSelect.selectedIndex].text,
+                neve: termekSelect.options[termekSelect.selectedIndex].text,
+                mennyiseg: Number(mennyisegUpDown.value),
+                egysegar: 0
+            };
+    
+            $rootScope.termekek.forEach(element => {
+                if (element.category == kategoriaSelect.options[kategoriaSelect.selectedIndex].text && element.productname == termekSelect.options[termekSelect.selectedIndex].text)
+                {
+                    termek.egysegar = Number(element.price);
+                }
+            });
+    
+            $rootScope.lista.push(termek);
+    
+            $rootScope.$apply();
+        }
+    });
+});
