31b19e41905072fb92298045418d299a5caef9b6
mojzeservin <113088620+mojzeservin@users.noreply.github.com>
2024-03-14 05:23:02 +0100

    törlés, módosítás

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+var mysql = require('mysql');
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT;
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : process.env.DBHOST,
+    user            : process.env.DBUSER,
+    password        : process.env.DBPASS,
+    database        : process.env.DBNAME
+});
+
+// MIDDLEWARES 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', function (req, res) {
+    pool.query(`SELECT * FROM termekek`, (error, results) => {
+        if (error) res.status(500).send(error);
+        res.status(200).send(results);
+    });
+});
+
+app.listen(port, ()=>{
+    console.log(`Server listening on port: ${port}...`);
+});
